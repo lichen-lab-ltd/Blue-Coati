ce7be009a28e89f76e1349cad9f674c9c5d89230
Wighawag <wighawag@gmail.com>
2020-07-18 11:06:22 +0100

    add nav component + wallet connect/disconnect

--- webapp/src/Root.svelte
+++ webapp/src/Root.svelte
@@ -2,13 +2,9 @@
   import {getResponse} from '@curi/svelte';
   const response = getResponse();
 
-  import Link from '@curi/svelte/components/Link.svelte';
-  import Button from './components/basic/Button.svelte';
+  import Nav from './components/Nav.svelte';
 </script>
 
-<nav class="flex justify-between">
-  <div><Link name="Index">Home</Link><Link name="Wallet">Wallet</Link></div>
-  <div><Button>Connect</Button></div>
-</nav>
+<Nav></Nav>
 
 <svelte:component this="{$response.body}" />
