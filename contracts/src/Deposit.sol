4b485998f28cdd21c2d22265c4c27c30cbf52d82
Wighawag <wighawag@gmail.com>
2020-07-22 21:35:44 +0100

    add ERC20 tokens

--- contracts/src/Deposit.sol
+++ contracts/src/Deposit.sol
@@ -3,6 +3,8 @@ pragma solidity 0.6.5;
 import "buidler-deploy/solc_0.6/proxy/Proxied.sol";
 import "@nomiclabs/buidler/console.sol";
 import "./Libraries/SafeMath.sol";
+import "./Interfaces/ERC20With2612.sol";
+import "./Interfaces/DAIERC20.sol";
 
 contract Deposit is Proxied {
     using SafeMath for uint256;
@@ -13,9 +15,38 @@ contract Deposit is Proxied {
     event Transfer(address indexed from, address indexed to, uint256 amount);
 
     function deposit(uint256 amount) external {
+        _deposit(msg.sender, amount);
+    }
+
+    function depositViaPermit(
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        _token.permit(msg.sender, address(this), amount, deadline, v, r, s);
+        _deposit(msg.sender, amount);
+    }
+
+    function depositViaDAIPermit(
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        DAIERC20 dai = DAIERC20(address(_token));
+        dai.permit(msg.sender, address(this), nonce, deadline, true, v, r, s);
+        _deposit(msg.sender, amount);
+    }
+
+    function _deposit(address from, uint256 amount) internal {
         require(_controller != address(0), "NOT_READY");
-        _deposits[msg.sender].amount = _deposits[msg.sender].amount.add(amount);
-        emit DepositReceived(msg.sender, amount);
+        require(_token.transferFrom(from, address(this), amount), "TRANSFER_FAILED");
+        _deposits[from].amount = _deposits[from].amount.add(amount);
+        emit DepositReceived(from, amount);
     }
 
     function takeControl() external {
@@ -30,17 +61,17 @@ contract Deposit is Proxied {
     ) external {
         require(msg.sender == _controller, "NOT_AUTHORIZED");
         _deposits[from].amount = _deposits[from].amount.sub(amount);
-        _deposits[to].amount = _deposits[to].amount.add(amount);
+        require(_token.transferFrom(address(this), to, amount), "TRANSFER_FAILED"); // work only on safe ERC20 token (no callback)
         emit Transfer(from, to, amount);
     }
 
     // ////////////////// CONSTRUCTOR /////////////////////////////
 
-    function postUpgrade(address token) public proxied {
+    function postUpgrade(ERC20With2612 token) public proxied {
         _token = token;
     }
 
-    constructor(address token) public {
+    constructor(ERC20With2612 token) public {
         postUpgrade(token);
     }
 
@@ -52,7 +83,7 @@ contract Deposit is Proxied {
     }
 
     /*TODO immutable (if no proxy)*/
-    address internal _token;
+    ERC20With2612 internal _token;
 
     address internal _controller;
     mapping(address => DepositInfo) internal _deposits;
