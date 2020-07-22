14e5beaf6d3b9852da23f402f1f9bb05a00c2334
Wighawag <wighawag@gmail.com>
2020-07-22 13:04:41 +0100

    add basic setup for Judgment losing bet

--- contracts/src/Deposit.sol
+++ contracts/src/Deposit.sol
@@ -2,16 +2,37 @@ pragma solidity 0.6.5;
 
 import "buidler-deploy/solc_0.6/proxy/Proxied.sol";
 import "@nomiclabs/buidler/console.sol";
+import "./Libraries/SafeMath.sol";
 
 contract Deposit is Proxied {
+    using SafeMath for uint256;
 
     event DepositReceived(address indexed from, uint256 amount);
-    
+
+    // TODO ERC20 ?
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
     function deposit(uint256 amount) external {
+        require(_controller != address(0), "NOT_READY");
         _deposits[msg.sender] = amount; // TODO
         emit DepositReceived(msg.sender, amount);
     }
 
+    function takeControl() external {
+        require(_controller == address(0) || msg.sender == _controller, "ALREADY_CONTROLLED");
+        _controller = msg.sender;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external {
+        require(msg.sender == _controller, "NOT_AUTHORIZED");
+        _deposits[from] = _deposits[from].sub(amount);
+        _deposits[to] = _deposits[to].add(amount);
+        emit Transfer(from, to, amount);
+    }
 
     // ////////////////// CONSTRUCTOR /////////////////////////////
 
@@ -25,6 +46,9 @@ contract Deposit is Proxied {
 
     // ///////////////////     DATA      //////////////////////////
 
+    /*TODO immutable (if no proxy)*/
     address internal _token;
+
+    address internal _controller;
     mapping(address => uint256) internal _deposits;
 }
--- contracts/src/Judgement.sol
+++ contracts/src/Judgement.sol
@@ -3,26 +3,45 @@ pragma solidity 0.6.5;
 import "buidler-deploy/solc_0.6/proxy/Proxied.sol";
 import "@nomiclabs/buidler/console.sol";
 import "./Deposit.sol";
+import "./Libraries/SigUtil.sol";
 
 contract Judgement is Proxied {
+    uint256 internal constant BET_AMOUNT = 1; // TODO
+
     function castJudgement(bytes32 documentId, bool accepted) external {
         require(msg.sender == _judge);
         require(_documents[documentId].judgementTime == 0, "ALREADY_DECIDED");
         _documents[documentId] = Document(uint64(block.timestamp), accepted);
     }
 
-    function claimReward(
+    function recordLosingBet(
         bytes32 documentId,
-        uint256 opositeBetId,
-        uint256 betId,
-        bool bet,
-        bytes calldata sig
+        uint256 losingBetId,
+        bytes calldata losingsig,
+        uint256 winningBetId,
+        uint256 chainBetId, // can be another losing bet // TODO chain it in a single call or can be 0 for document bet
+        bytes calldata winningSig
     ) external {
-        require(_documents[documentId].judgementTime > 0, "JUDGEMENT_NOT_EXIST");
+        require(_documents[documentId].judgementTime > 0, "JUDGEMENT_DOESNT_EXIST");
         require(uint64(block.timestamp) < _documents[documentId].judgementTime + _claimPeriod, "TOO_LATE");
-        require(_betsCounted[documentId][betId] == false, "ALREADY_SUBMITED");
-        // TODO
-        _betsCounted[documentId][betId] = true;
+        require(_betsCounted[documentId][losingBetId] == false, "ALREADY_SUBMITED");
+
+        bool result = _documents[documentId].result;
+
+        _betsCounted[documentId][losingBetId] = true;
+
+        address loser = SigUtil.recover(
+            keccak256(abi.encode(documentId, losingBetId, winningBetId, !result)),
+            losingsig
+        );
+
+        address winner;
+        if (winningBetId == 0) {
+            winner = SigUtil.recover(keccak256(abi.encode(documentId, winningBetId, chainBetId, result)), winningSig);
+            _deposit.transferFrom(loser, winner, BET_AMOUNT);
+        } else {
+            _deposit.transferFrom(loser, address(this), BET_AMOUNT);
+        }
     }
 
     // ////////////////// CONSTRUCTOR /////////////////////////////
@@ -30,11 +49,12 @@ contract Judgement is Proxied {
     function postUpgrade(address judge, Deposit deposit) public proxied {
         _judge = judge;
         _deposit = deposit;
+        _deposit.takeControl();
     }
 
-    constructor(address judge, Deposit deposit) public {
-        postUpgrade(judge, deposit);
-    }
+    // constructor(address judge, Deposit deposit) public {
+    //     postUpgrade(judge, deposit);
+    // }
 
     // ///////////////////     DATA      //////////////////////////
 
@@ -44,10 +64,10 @@ contract Judgement is Proxied {
     }
     mapping(bytes32 => mapping(uint256 => bool)) _betsCounted;
 
-    /*immutable (if no proxy)*/
+    /*TODO immutable (if no proxy)*/
     uint64 internal _claimPeriod = 7 days; // TODO constructor
 
-    /*immutable (if no proxy)*/
+    /*TODO immutable (if no proxy)*/
     Deposit internal _deposit;
     address _judge;
     mapping(bytes32 => Document) _documents;
