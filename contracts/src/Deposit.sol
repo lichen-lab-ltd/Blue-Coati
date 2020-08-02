f20e03fc7819ee57996c9e2333051bd64a7ef738
Wighawag <wighawag@gmail.com>
2020-08-02 20:29:54 +0100

    keep ERC20 code for later

--- contracts/src/Deposit.sol
+++ contracts/src/Deposit.sol
@@ -20,6 +20,44 @@ contract Deposit is Proxied {
         emit DepositReceived(msg.sender, msg.value);
     }
 
+    // TODO DAI ERC20 : 
+    /*
+    function deposit(uint256 amount) external {
+        _deposit(msg.sender, amount);
+    }
+
+    function depositViaPermit(
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        _token.permit(msg.sender, address(this), amount, deadline, v, r, s);
+        _deposit(msg.sender, amount);
+    }
+
+    function depositViaDAIPermit(
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        DAIERC20 dai = DAIERC20(address(_token));
+        dai.permit(msg.sender, address(this), nonce, deadline, true, v, r, s);
+        _deposit(msg.sender, amount);
+    }
+    
+    function _deposit(address from, uint256 amount) internal {
+        require(_controller != address(0), "NOT_READY");
+        require(_token.transferFrom(from, address(this), amount), "TRANSFER_FAILED");
+        _deposits[from].amount = _deposits[from].amount.add(amount);
+        emit DepositReceived(from, amount);
+    }
+    */
+
     function takeControl() external {
         require(_controller == address(0) || msg.sender == _controller, "ALREADY_CONTROLLED");
         _controller = msg.sender;
